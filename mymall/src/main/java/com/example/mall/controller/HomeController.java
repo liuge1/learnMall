7370e4f97b37a8d4be77546c669a0c5373a930e5
liuge1 <xiaocai_liu@163.com>
2020-06-19 20:04:54 +0800

    启动项目

---
+++ mymall/src/main/java/com/example/mall/controller/HomeController.java
@@ -0,0 +1,11 @@
+package com.example.mall.controller;
+
+/**
+ * @Classname HomeController
+ * @Description TODO
+ * @Date 2020/6/19 19:54
+ * @Created by v_geeliu
+ */
+public class HomeController {
+
+}
