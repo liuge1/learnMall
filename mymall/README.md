81cfbdbb2c59749f4aa31b1a5d5910a45fceee6a
liuge1 <xiaocai_liu@163.com>
2020-08-18 20:37:33 +0800

    整合redis

--- mymall/README.md
+++
@@ -1,6 +0,0 @@
-1. classpath 和 classpath* 的区别
-classpath: 和classpath*: 在spring加载资源的时候是不同的
-classpath: 只能加载找到的第一个资源文件(上面只匹配了com/eloan/bu/mapper/business下的mapper文件,而com/eloan/base/mapper就陪忽略了)
-classpath*: 能加载多个路径下的资源文件.(com/eloan/bu/mapper/business和com/eloan/base/mapper都被加载进来了.)
-
-2.整合swagger
---
+++ mymall/src/main/java/com/example/mall/service/RedisService.java
@@ -0,0 +1,37 @@
+package com.example.mall.service;
+
+/**
+ * @Classname RedisService
+ * @Description redis操作Service,对象和数组都以json形式进行存储
+ * @Date 2020/8/18 20:35
+ * @Created by v_geeliu
+ */
+public interface RedisService {
+
+  /**
+   * 存储数据
+   */
+  void set(String key, String value);
+
+  /**
+   * 获取数据
+   */
+  String get(String key);
+
+  /**
+   * 设置超期时间
+   */
+  boolean expire(String key, long expire);
+
+  /**
+   * 删除数据
+   */
+  void remove(String key);
+
+  /**
+   * 自增操作
+   *
+   * @param delta 自增步长
+   */
+  Long increment(String key, long delta);
+}
