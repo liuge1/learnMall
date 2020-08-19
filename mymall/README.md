31a824486eff24e3578143d5ac61f69bc96cd582
liuge1 <xiaocai_liu@163.com>
2020-08-19 14:25:19 +0800

    JWT

--- mymall/README.md
+++ mymall/README.md
@@ -9,3 +9,13 @@
   * https://github.com/MicrosoftArchive/redis/releases
   * 找打目录 在当前地址栏输入cmd
   * 执行redis的启动命令：redis-server.exe redis.windows.conf  
+  
+4. SpringSecurity 和 JWT
+   * https://jwt.io/
+   * Hutool 工具包帮助我们简化每一行代码，减少每一个方法
+   ums_admin：后台用户表
+   ums_role：后台用户角色表
+   ums_permission：后台用户权限表
+   ums_admin_role_relation：后台用户和角色关系表，用户与角色是多对多关系
+   ums_role_permission_relation：后台用户角色和权限关系表，角色与权限是多对多关系
+   ums_admin_permission_relation：后台用户和权限关系表(除角色中定义的权限以外的加减权限)，加权限是指用户比角色多出的权限，减权限是指用户比角色少的权限
---
+++ mymall/src/main/java/com/example/mall/common/utils/JwtTokenUtil.java
@@ -0,0 +1,137 @@
+package com.example.mall.common.utils;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.SignatureException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Classname JwtTokenUtil
+ * @Description TODO
+ * @Date 2020/8/19 11:13
+ * @Created by v_geeliu
+ */
+@Slf4j
+@Component
+public class JwtTokenUtil {
+
+  private static final String CLAIM_KEY_USERNAME = "sub";
+  private static final String CLAIM_KEY_CREATED = "created";
+  @Value("${jwt.secret}")
+  private String secret;
+  @Value("${jwt.expiration}")
+  private Long expiration;
+
+  /**
+   * 根据负载生成JWT的token
+   */
+  private String generateToken(UserDetails userDetails) {
+    Map<String, Object> claims = new HashMap<>();
+    claims.put(CLAIM_KEY_USERNAME, userDetails.getUsername());
+    claims.put(CLAIM_KEY_CREATED, new Date());
+    return generateToken(claims);
+  }
+
+  /**
+   * 根据用户信息的token
+   */
+  private String generateToken(Map<String, Object> claims) {
+    return Jwts.builder()
+        .setClaims(claims)
+        .setExpiration(generateExpirationDate())
+        .signWith(SignatureAlgorithm.HS512, secret)
+        .compact();
+  }
+
+  /**
+   * 从token中获取JWT中的负载
+   */
+  private Claims getClaimsFromToken(String token) {
+    Claims claims = null;
+    try {
+      Jwts.parser()
+          .setSigningKey(secret)
+          .parseClaimsJws(token)
+          .getBody();
+    } catch (Exception e) {
+      e.printStackTrace();
+      log.error("JWT格式验证失败:{}",token);
+    }
+    return claims;
+  }
+
+  /**
+   * 从token中获取登录用户名
+   * */
+  public String getUserNameFromToken(String token) {
+    String userName;
+    try {
+      Claims claims = getClaimsFromToken(token);
+      userName=claims.getSubject();
+    } catch (Exception e) {
+      e.printStackTrace();
+      userName=null;
+    }
+    return userName;
+  }
+
+  /**
+   * @param token 客户端传入的token
+   * @param  userDetails 从数据中查询出来的用户的信息
+   * */
+  public boolean validateToken(String token, UserDetails userDetails) {
+    String username = getUserNameFromToken(token);
+    return username.equals(userDetails.getUsername())&&!isTokenExpired(token);
+  }
+
+  /**
+   * 判断token是否已经失效
+   * */
+  private boolean isTokenExpired(String token){
+    Date expiredDate = getExpiredDateFromToken(token);
+    return expiredDate.before(new Date());
+  }
+
+  /**
+   * 从token中获取过期时间
+   * */
+  private  Date getExpiredDateFromToken(String token){
+    Claims claims = getClaimsFromToken(token);
+    return claims.getExpiration();
+  }
+
+
+  /**
+   * 生成token的过期时间
+   */
+  private Date generateExpirationDate() {
+    return new Date(System.currentTimeMillis() + expiration * 1000);
+  }
+
+  /**
+   * 判断token是否可以被刷新
+   */
+  public boolean canRefresh(String token) {
+    return !isTokenExpired(token);
+  }
+
+  /**
+   * 刷新token
+   */
+  public String refreshToken(String token) {
+    Claims claims = getClaimsFromToken(token);
+    claims.put(CLAIM_KEY_CREATED, new Date());
+    return generateToken(claims);
+  }
+
+}
